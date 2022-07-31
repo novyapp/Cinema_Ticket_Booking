8b919f98bf283fc793a1e6f1de923af8383b1dc3
Michał Nowotnik <micnowotnik@gmail.com>
2022-07-31 13:57:48 +0200

    working generator

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -6,19 +6,13 @@ export default function Seat({
   seatsIn,
   handleSelectedState,
 }) {
-  console.log("seat", movie);
-
-  const createSeats = (
-    rows: number,
-    startIndex: number | any,
-    endIndex: string
-  ) => {
+  const createSeats = (rows: number, startIndex: number, endIndex: string) => {
     let i = 0;
     let j = startIndex;
     let k = "A";
     let eI = endIndex + 1;
     const section = [];
-    while (i < 6 && j <= rows) {
+    while (j <= rows) {
       if (k > eI) {
         k = "A";
         j++;
@@ -30,43 +24,58 @@ export default function Seat({
     }
     return section;
   };
-  function intToChar(int) {
+  function intToChar(int: number) {
     const code = "A".charCodeAt(0);
     return String.fromCharCode(code + int);
   }
 
   return (
     <div className="flex flex-col items-center space-y-8">
-      {seatsIn.map((seatsTypes) => {
-        const nti = intToChar(seatsTypes.numberOfSeats - 1);
-        const hallSeaction = createSeats(seatsTypes.numberOfRows, "1", nti);
+      {seatsIn.map(
+        (seatsTypes: {
+          numberOfSeats: number;
+          numberOfRows: number;
+          rowsIndex: number;
+          id: React.Key | null | undefined;
+        }) => {
+          const nti = intToChar(seatsTypes.numberOfSeats - 1);
+          const hallSeaction = createSeats(
+            seatsTypes.numberOfRows,
+            seatsTypes.rowsIndex,
+            nti
+          );
 
-        return (
-          <div className={`grid grid-cols-${seatsTypes.numberOfSeats} gap-2`}>
-            {/* <div>{seatsTypes.name}</div> */}
-            {hallSeaction.map((seat) => {
-              const isSelected = selectedSeats.includes(seat);
-              const isOccupied = movie?.takenSeats.includes(seat);
+          return (
+            <div
+              key={seatsTypes.id}
+              className={`grid grid-cols-${seatsTypes.numberOfSeats} gap-2`}
+            >
+              {hallSeaction.map((seat, i) => {
+                const isSelected = selectedSeats.includes(seat);
+                const isOccupied = movie?.takenSeats.includes(seat);
 
-              return (
-                <span
-                  key={seat}
-                  className={`bg-zinc-300 w-10 h-10 text-zinc-500 flex items-center justify-center font-semibold rounded-md ${
-                    isSelected &&
-                    "bg-gradient-to-t from-pink-700 to-pink-500 !text-white "
-                  } ${
-                    isOccupied &&
-                    "!bg-zinc-800 pointer-events-none !text-zinc-700"
-                  }`}
-                  onClick={isOccupied ? null : () => handleSelectedState(seat)}
-                >
-                  {seat}
-                </span>
-              );
-            })}
-          </div>
-        );
-      })}
+                return (
+                  <span
+                    key={seat}
+                    className={`bg-zinc-300 w-10 h-10 text-zinc-500 flex items-center justify-center font-semibold rounded-md ${
+                      isSelected &&
+                      "bg-gradient-to-t from-pink-700 to-pink-500 !text-white "
+                    } ${
+                      isOccupied &&
+                      "!bg-zinc-800 pointer-events-none !text-zinc-700"
+                    }`}
+                    onClick={
+                      isOccupied ? null : () => handleSelectedState(seat)
+                    }
+                  >
+                    {seat}
+                  </span>
+                );
+              })}
+            </div>
+          );
+        }
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,179 +1,208 @@
+import { MovieSeance } from "@prisma/client";
 import React, { useEffect, useState } from "react";
-import { Mutation } from "react-query";
+import Seat from "../components/Seat";
 import Seats from "../components/Seats";
 import { trpc } from "../utils/trpc";
 
-const createSeats = (rows, startIndex, endIndex) => {
-  let i = 0;
-  let j = startIndex;
-  let k = "A";
-  let eI = endIndex + 1;
-  const section = [];
-  while (i < 6 && j <= rows) {
-    if (k > eI) {
-      k = "A";
-      j++;
-    }
-    if (j < rows + 1) {
-      section.push(j + k);
-      k = String.fromCharCode(k.charCodeAt(0) + 1);
-    }
-  }
-  return section;
-};
-
-const movies = [
-  {
-    name: "Avenger",
-
-    occupied: ["4F", "4E", "4D", "4C", "2C", "2B"],
-    cinema: {
-      premiumRows: 2,
-      premiumSeatsInRow: 6,
-      normalRows: 6,
-      normalSeatsInRow: 8,
-    },
-  },
-];
-
-export default function index() {
-  const { data: movie, isLoading } = trpc.useQuery([
-    "movie.get-movie",
-    { id: "cl656hnzb0154jgvnhkmlhfcg" },
-  ]);
-  const { data: MovieSeance } = trpc.useQuery([
-    "movie.get-movie-seanse",
-    { id: "cl656kn4g0215jgvn5ubntljx" },
-  ]);
-  const updateseats = trpc.useMutation("movie.update-movie");
-
-  //console.log(MovieSeance);
-
-  function intToChar(int) {
-    const code = "A".charCodeAt(0);
-    return String.fromCharCode(code + int);
-  }
-
-  const premiumseatsInRow = movies[0]?.cinema.premiumSeatsInRow;
-  const premiumRows = movies[0]?.cinema.premiumRows;
-  const nsl = intToChar(premiumseatsInRow);
-  const premiumSeats = createSeats(premiumRows, "1", nsl);
-
-  const normalseatsInRow = movies[0]?.cinema.normalSeatsInRow;
-  const normalRows = movies[0]?.cinema.normalRows;
-  const psl = intToChar(normalseatsInRow);
-  const normalSeats = createSeats(normalRows, "3", psl);
-
-  const [availableSeats, setAvailableSeats] = useState([]);
-  const [bookedSeats, setBookedSeats] = useState([]);
-  const time = movie?.MovieSeance[0]?.date.toLocaleTimeString();
-  const [selectedTime, setSelectedTime] = useState();
-
-  useEffect(() => {
-    if (movie) {
-      setAvailableSeats(movie.MovieSeance[0]?.takenSeats);
-      setSelectedTime(movie.MovieSeance[0]);
-    }
-  }, [movie]);
-  //console.log(movie?.MovieSeance[0]?.takenSeats);
-  //console.log(availableSeats);
-
-  const addSeat = (ev) => {
-    if (numberOfSeats && !ev.target.className.includes("disabled")) {
-      const seatsToBook = parseInt(numberOfSeats, 10);
-      if (bookedSeats.length <= seatsToBook) {
-        if (bookedSeats.includes(ev.target.innerText)) {
-          const newAvailable = bookedSeats.filter(
-            (seat) => seat !== ev.target.innerText
-          );
-          setBookedSeats(newAvailable);
-        } else if (bookedSeats.length < numberOfSeats) {
-          setBookedSeats([...bookedSeats, ev.target.innerText]);
-        } else if (bookedSeats.length === seatsToBook) {
-          bookedSeats.shift();
-          setBookedSeats([...bookedSeats, ev.target.innerText]);
-        }
+interface CinemaType {
+  movie: object | undefined;
+  numberOfSeats: string | number;
+  selectedSeats: string[];
+  onSelectedSeatsChange: Function;
+}
+interface MoviesType {
+  movie:
+    | {
+        id: string;
+        name: string;
+        takenSeats: string[] | undefined;
       }
-    }
-  };
+    | null
+    | undefined;
+  onChange: (movie: {
+    id: string;
+    name: string;
+    takenSeats: string[] | undefined;
+    date: string;
+    cinemaHallId: string;
+    movieId: string;
+  }) => void;
+}
 
-  const confirmBooking = () => {
-    const newAvailableSeats = [...availableSeats, ...bookedSeats];
-    setAvailableSeats(newAvailableSeats);
-    setBookedSeats([]);
-    updateseats.mutate({
-      id: movie?.MovieSeance[0]?.id,
-      seats: bookedSeats,
-    });
-  };
+export default function App() {
+  const { data: cinema, isLoading } = trpc.useQuery([
+    "cinema.get-halls",
+    { id: "cl66dr7gt0031sovnv5ib7wsu" },
+  ]);
+  const utils = trpc.useContext();
+  const updateseats = trpc.useMutation("movie.update-movie", {
+    onSuccess() {
+      utils.invalidateQueries("cinema.get-halls", {
+        id: "cl66dr7gt0031sovnv5ib7wsu",
+      });
+    },
+  });
+  const deftime = cinema?.cinemaHall[0]?.movieSeance[0];
+  const seats = cinema?.cinemaHall[0]?.seats;
+
+  console.log(seats);
+
+  const [selectedMovie, setSelectedMovie] = useState<MovieSeance | undefined>(
+    deftime
+  );
+  const [selectedSeats, setSelectedSeats] = useState([]);
   const [numberOfSeats, setNumberOfSeats] = useState(2);
 
-  function Movies({ seanseTime, onChange }) {
-    //console.log("SeanseTime", seanseTime);
+  useEffect(() => {
+    setSelectedMovie(deftime);
+  }, [isLoading]);
+
+  const Movies = ({ movie, onChange }: MoviesType) => {
     return (
-      <div className="Movies">
+      <div className="flex justify-center items-center space-x-4">
+        <label htmlFor="movie">Pick a time:</label>
         <select
+          className="text-zinc-200 bg-zinc-700 p-2 rounded-md"
           id="movie"
-          value={seanseTime?.id}
+          value={movie?.id}
           onChange={(e) => {
             onChange(
-              movie?.MovieSeance.find((movie) => movie.id === e.target.value)
+              cinema?.cinemaHall[0]?.movieSeance.find(
+                (mov) => mov.id === e.target.value
+              )
             );
           }}
         >
-          {movie?.MovieSeance.map((time) => (
-            <option key={time.id} value={time.id}>
-              {movie.title} at {time.date.toLocaleTimeString()}
+          {cinema?.cinemaHall[0]?.movieSeance.map((mov) => (
+            <option
+              key={mov.id}
+              value={mov.id}
+              className="text-zinc-200 bg-zinc-700"
+            >
+              {mov.date.toLocaleTimeString()}
             </option>
           ))}
         </select>
+        <label>Number of tickets: </label>
+
+        <input
+          className="text-zinc-200 bg-zinc-700 p-2 rounded-md w-20"
+          type="number"
+          value={numberOfSeats}
+          onChange={(ev) =>
+            setNumberOfSeats(ev.target.value as unknown as number)
+          }
+        />
       </div>
     );
+  };
+
+  const confirmBooking = () => {
+    const newAvailableSeats = [...selectedMovie.takenSeats, ...selectedSeats];
+    console.log("new seats", newAvailableSeats);
+    setSelectedMovie((mov) =>
+      mov?.id ? { ...mov, takenSeats: newAvailableSeats } : mov
+    );
+    updateseats.mutate({
+      id: selectedMovie?.id,
+      seats: selectedSeats,
+    });
+
+    setSelectedSeats([]);
+  };
+  if (!cinema && isLoading) return null;
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gradient-to-t from-slate-800 via-slate-900 to-black/90 items-center space-y-6 ">
+      <Movies
+        movie={selectedMovie}
+        onChange={(movie) => {
+          setSelectedSeats([]);
+          setSelectedMovie(movie);
+        }}
+      />
+
+      <ShowCase />
+      <Cinema
+        seatsIn={seats}
+        movie={selectedMovie}
+        numberOfSeats={numberOfSeats}
+        selectedSeats={selectedSeats}
+        onSelectedSeatsChange={(selectedSeats: React.SetStateAction<never[]>) =>
+          setSelectedSeats(selectedSeats)
+        }
+      />
+      <button
+        onClick={confirmBooking}
+        className="rounded-md p-2 bg-gradient-to-b from-pink-500 to bg-pink-600 font-semibold text-white w-32"
+      >
+        Book seats
+      </button>
+    </div>
+  );
+}
+
+function ShowCase() {
+  return (
+    <ul className="flex items-center justify-center space-x-4 py-6">
+      <li className="flex flex-col items-center">
+        <span className="flex rounded-md  h-10 bg-zinc-400 w-10 text-zinc-500" />
+        <small>N/A</small>
+      </li>
+      <li className="flex flex-col items-center">
+        <span className="flex rounded-md w-10 h-10 text-zinc-500 bg-gradient-to-t from-pink-700 to-pink-500" />
+        <small>Selected</small>
+      </li>
+      <li className="flex flex-col items-center">
+        <span className="flex rounded-md w-10 h-10 bg-zinc-800 pointer-events-none text-zinc-700" />
+        <small>Occupied</small>
+      </li>
+    </ul>
+  );
+}
+
+function Cinema({
+  movie,
+  seatsIn,
+  numberOfSeats,
+  selectedSeats,
+  onSelectedSeatsChange,
+}: CinemaType) {
+  function handleSelectedState(seat: string) {
+    const seatsToBook = parseInt(numberOfSeats as string, 10);
+    const isSelected = selectedSeats.includes(seat);
+
+    const test = numberOfSeats;
+    console.log(test);
+
+    if (seatsToBook >= selectedSeats.length) {
+      if (isSelected) {
+        onSelectedSeatsChange(
+          selectedSeats.filter((selectedSeat: string) => selectedSeat !== seat)
+        );
+      } else if (selectedSeats.length === seatsToBook) {
+        selectedSeats.shift();
+        onSelectedSeatsChange([...selectedSeats, seat]);
+      } else {
+        onSelectedSeatsChange([...selectedSeats, seat]);
+      }
+    } else if (seatsToBook < selectedSeats.length) {
+      const slice = selectedSeats.slice(0, seatsToBook);
+      slice.shift();
+      onSelectedSeatsChange([...slice, seat]);
+    }
   }
-
-  if (!movie && isLoading) return null;
-  if (movie && !isLoading)
-    return (
-      <div className="flex flex-col items-center w-full space-y-2">
-        {!isLoading && (
-          <>
-            <Movies
-              seanseTime={selectedTime}
-              onChange={(selectedTime) => {
-                setBookedSeats([]);
-                setSelectedTime(selectedTime);
-              }}
-            />
-
-            <div>
-              <h2>Booking ticket for at:</h2>
-              <div className="bg-zinc-600 p-2 text-white"></div>
-              <input
-                className="bg-zinc-50 border-2 p-2"
-                type="number"
-                value={numberOfSeats}
-                onChange={(ev) => setNumberOfSeats(ev.target.value)}
-              />
-              <Seats
-                values={premiumSeats}
-                seanseTime={selectedTime}
-                seatsRow={premiumseatsInRow}
-                availableSeats={availableSeats}
-                bookedSeats={bookedSeats}
-                addSeat={addSeat}
-              />
-              <Seats
-                values={normalSeats}
-                seanseTime={selectedTime}
-                seatsRow={normalseatsInRow}
-                availableSeats={availableSeats}
-                bookedSeats={bookedSeats}
-                addSeat={addSeat}
-              />
-              <button onClick={confirmBooking}>Book seats</button>
-            </div>
-          </>
-        )}
+  if (!movie) return null;
+  return (
+    <>
+      <div className="flex flex-col items-center space-y-2">
+        <Seat
+          seatsIn={seatsIn}
+          movie={movie}
+          handleSelectedState={handleSelectedState}
+          selectedSeats={selectedSeats}
+        />
       </div>
-    );
+    </>
+  );
 }
